292b0aeab19b29bfb61a4bf3e6e584d750fa60d4
gogerch <gogerch@gmail.com>
2023-05-18 15:21:28 +0300

    отсортировал по году выпуска и мощности4

--- src/main/kotlin/break continue.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    var i = 0
-    while (i < 5) {
-        val n = readln().toDouble()
-        if (n < 0)
-            continue
-        println(sqrt(n))
-        i++
-    }
---
+++ src/main/kotlin/Типы данных.kt
@@ -0,0 +1,17 @@
+//import kotlin.random.Random
+//fun main() {
+//    println(Random.nextDouble())
+//    println(Random.nextDouble(-2.0, 2.0))
+//}
+
+//fun main() {
+//    println(Byte.SIZE_BYTES)
+//    println(Short.SIZE_BYTES)
+//    println(Int.SIZE_BYTES)
+//    println(Long.SIZE_BYTES)
+//}
+
+fun main() {
+    println((1 + 1.0) is Double) // true
+    println((1L + 1) is Long) // true
+}
